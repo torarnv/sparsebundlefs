cb7bbdffee2c93181bff562ba0e45c1262bbe49a
Tor Arne Vestbø <tor.arne.vestbo@qt.io>
2018-09-13 16:36:47 +0200

    Convert tests indentation to spaces

--- tests/10_basic.sh
+++ tests/10_basic.sh
@@ -1,36 +1,36 @@
 
 function setup() {
-	mount_dir=$(mktemp -d)
-	$SPARSEBUNDLEFS -s -f -D $TEST_BUNDLE $mount_dir &
-	for i in {0..50}; do
-		# FIXME: Find actual mount callback in fuse?
-		grep -q "bundle has" $test_output_file && break || sleep 0.1
-	done
-	pid=$!
-	dmg_file=$mount_dir/sparsebundle.dmg
+    mount_dir=$(mktemp -d)
+    $SPARSEBUNDLEFS -s -f -D $TEST_BUNDLE $mount_dir &
+    for i in {0..50}; do
+        # FIXME: Find actual mount callback in fuse?
+        grep -q "bundle has" $test_output_file && break || sleep 0.1
+    done
+    pid=$!
+    dmg_file=$mount_dir/sparsebundle.dmg
 }
 
 function test_dmg_exists_after_mounting() {
-	ls -al $mount_dir
-	test -f $dmg_file
+    ls -al $mount_dir
+    test -f $dmg_file
 }
 
 function test_dmg_has_expected_size() {
-	size=$(ls -dn $dmg_file | awk '{print $5; exit}')
-	test $size -eq 1099511627776
+    size=$(ls -dn $dmg_file | awk '{print $5; exit}')
+    test $size -eq 1099511627776
 }
 
 function test_dmg_has_correct_owner() {
-	owner=$(ls -l $dmg_file | awk '{print $3; exit}')
-	test $owner = $(whoami)
+    owner=$(ls -l $dmg_file | awk '{print $3; exit}')
+    test $owner = $(whoami)
 }
 
 function test_dmg_has_correct_permissions() {
-	permissions=$(ls -l $dmg_file | awk '{print $1; exit}')
-	test $permissions = "-r--------"
+    permissions=$(ls -l $dmg_file | awk '{print $1; exit}')
+    test $permissions = "-r--------"
 }
 
 function teardown() {
-	umount $mount_dir
-	rm -Rf $mount_dir
+    umount $mount_dir
+    rm -Rf $mount_dir
 }
