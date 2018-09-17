a5ad123db1487da452398fd5f1ab5e2180097da4
Tor Arne Vestbø <torarnv@gmail.com>
2018-09-18 00:19:17 +0200

    Look for sparsebundle.dmg when setting up tests

--- tests/testhelpers.sh
+++ tests/testhelpers.sh
@@ -2,12 +2,11 @@
 function mount_sparsebundle() {
     test ! -z "$TEST_BUNDLE"
     mount_dir=$(mktemp -d)
+    dmg_file="$mount_dir/sparsebundle.dmg"
     sparsebundlefs -s -f -D $* $TEST_BUNDLE $mount_dir &
     pid=$!
     for i in {0..50}; do
         kill -0 $pid >/dev/null 2>&1
-        # FIXME: Find actual mount callback in fuse?
-        grep -q "bundle has" $test_output_file && break || sleep 0.1
+        test -f $dmg_file && break || sleep 0.1
     done
-    dmg_file=$mount_dir/sparsebundle.dmg
 }
