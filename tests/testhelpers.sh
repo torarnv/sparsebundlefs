d6fb55b81fce3810755b5fc6e6d9bc439ae16c63
Tor Arne Vestbø <torarnv@gmail.com>
2021-07-07 16:59:52 +0200

    Fix CodeFactor issue

--- tests/testhelpers.sh
+++ tests/testhelpers.sh
@@ -25,7 +25,8 @@ function _test_dmg_has_correct_number_of_blocks() {
 }
 
 function _test_dmg_contents_is_same_as_testdata() {
-    for f in $(ls $HFSFUSE_DIR/src); do
+    for f in $HFSFUSE_DIR/src/*; do
+        f=$(basename $f)
         echo "Diffing $HFSFUSE_DIR/src/$f"
         diff $HFSFUSE_DIR/src/$f <(hfsdump $dmg_file read "/src/$f")
     done
